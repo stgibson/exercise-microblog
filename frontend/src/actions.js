01d7811270ff29eed52ef69b78b86b9d55cc62b3
Sean Gibson <seanthomasgibson@gmail.com>
2021-04-07 14:21:58 -0700

    Added and modified comments in 1 file. Modified frontend/src/actions.js.

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -12,14 +12,28 @@ import {
 
 const BASE_URL = "http://localhost:5000/api/posts";
 
+/**
+ * Creates action to add an error in redux store
+ * @param {string} msg 
+ * @returns action
+ */
 const showErr = (msg) => {
   return { type: SHOW_ERR, msg };
 };
 
+/**
+ * Creates action to load titles in redux store
+ * @param {Object{Object{string}}} titles 
+ * @returns action
+ */
 const getTitles = (titles) => {
   return { type: LOAD_TITLES, titles };
 };
 
+/**
+ * Creates thunk that gets titles from backend before adding them to redux store
+ * @returns thunk
+ */
 const getTitlesFromAPI = () => {
   return async dispatch => {
     try {
@@ -39,10 +53,21 @@ const getTitlesFromAPI = () => {
   };
 };
 
+/**
+ * Creates action to load a post in redux store
+ * @param {string} id 
+ * @param {Object{string|Object{string}}} post 
+ * @returns action
+ */
 const getPost = (id, post) => {
   return { type: LOAD_POST, id, post };
 };
 
+/**
+ * Creates thunk that gets post from backend before adding it to redux store
+ * @param {string} id 
+ * @returns thunk
+ */
 const getPostFromAPI = (id) => {
   return async dispatch => {
     try {
@@ -61,7 +86,7 @@ const getPostFromAPI = (id) => {
 };
 
 /**
- * Gets action to add post
+ * Creates action to add post in redux store
  * @param {string} id 
  * @param {Object{string|Object{string}}} post 
  * @returns action
@@ -75,6 +100,11 @@ const addPost = (id, post) => {
   };
 };
 
+/**
+ * Creates thunk that adds post to db before adding it to redux store
+ * @param {Object{string|Object{string}}} post 
+ * @returns thunk
+ */
 const addPostToAPI = (post) => {
   return async dispatch => {
     try {
@@ -88,7 +118,7 @@ const addPostToAPI = (post) => {
 }
 
 /**
- * Gets action to edit post
+ * Creates action to edit post in redux store
  * @param {string} id 
  * @param {Object{string|Object{string}}} post 
  * @returns action
@@ -102,6 +132,12 @@ const editPost = (id, post) => {
   };
 };
 
+/**
+ * Creates thunk that edits post in db before editing it in redux store
+ * @param {string} id 
+ * @param {Object{string|Object{string}}} post 
+ * @returns thunk
+ */
 const editPostInAPI = (id, post) => {
   return async dispatch => {
     try {
@@ -115,7 +151,7 @@ const editPostInAPI = (id, post) => {
 }
 
 /**
- * Gets action to delete post
+ * Creates action to delete post in redux store
  * @param {string} id 
  * @returns action
  */
@@ -123,6 +159,11 @@ const deletePost = (id) => {
   return { type: DELETE_POST, id };
 };
 
+/**
+ * Creates thunk that deletes post in db before deleting it in redux store
+ * @param {string} id 
+ * @returns thunk
+ */
 const deletePostInAPI = (id) => {
   return async dispatch => {
     try {
@@ -136,7 +177,7 @@ const deletePostInAPI = (id) => {
 };
 
 /**
- * Gets action to add comment
+ * Creates action to add comment in redux store
  * @param {string} postId 
  * @param {string} commentId 
  * @param {string} comment 
@@ -146,6 +187,12 @@ const addComment = (postId, commentId, comment) => {
   return { type: ADD_COMMENT, postId, commentId, comment };
 };
 
+/**
+ * Creates thunk that adds comment to db before adding it in redux store
+ * @param {string} postId 
+ * @param {string} comment 
+ * @returns thunk
+ */
 const addCommentToAPI = (postId, comment) => {
   return async dispatch => {
     try {
@@ -160,7 +207,7 @@ const addCommentToAPI = (postId, comment) => {
 };
 
 /**
- * Gets action to delete comment
+ * Creates action to delete comment in redux store
  * @param {string} postId 
  * @param {string} commentId 
  * @returns action
@@ -169,6 +216,12 @@ const deleteComment = (postId, commentId) => {
   return { type: DELETE_COMMENT, postId, commentId };
 };
 
+/**
+ * Creates thunk that deletes comment in db before deleting it in redux store
+ * @param {string} postId 
+ * @param {string} commentId 
+ * @returns thunk
+ */
 const deleteCommentInAPI = (postId, commentId) => {
   return async dispatch => {
     try {
