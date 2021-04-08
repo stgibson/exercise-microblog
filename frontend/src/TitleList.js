c469021672fbaff90d018f0258fb1c37eb417835
Sean Gibson <seanthomasgibson@gmail.com>
2021-04-07 17:36:18 -0700

    Sorting titles by votes. Added compareTitleIds(titleId1, titleId2) & sortTitleIds() in frontend/src/TitleList.js.

--- frontend/src/TitleList.js
+++ frontend/src/TitleList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, shallowEqual } from "react-redux";
 import { v4 as uuid } from "uuid";
 import Row from "react-bootstrap/Row";
@@ -11,11 +11,45 @@ import Title from "./Title";
  */
 const TitleList = () => {
   const titles = useSelector(store => store.titles, shallowEqual);
+  const [titleIdsSorted, setTitleIdsSorted] = useState([]);
+
+  /**
+   * Compares title ids based on votes of their respective titles
+   * @param {string} titleId1 
+   * @param {string} titleId2 
+   * @returns 1 if votes for title with id titleId1 < votes for title with id
+   * title2, -1 if votes for title with id titleId1 > votes for title with id
+   * title2, or 0 otherwise
+   */
+  const compareTitleIds = (titleId1, titleId2) => {
+    const title1 = titles[titleId1];
+    const title2 = titles[titleId2];
+    if (title1.votes < title2.votes) {
+      return 1;
+    }
+    if (title1.votes > title2.votes) {
+      return -1;
+    }
+    return 0;
+  };
+
+  /**
+   * Sorts title ids based on votes of their respective titles
+   */
+  const sortTitleIds = () => {
+    const titleIds = Object.keys(titles);
+    titleIds.sort(compareTitleIds); // Learned how to use sort at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    setTitleIdsSorted(titleIds);
+  };
+
+  useEffect(() => {
+    sortTitleIds();
+  }, [titles]);
 
   return (
     <Row>
       {
-        Object.keys(titles).map(titleId => (
+        titleIdsSorted.map(titleId => (
           <Col key={ uuid() } xs={ 6 }>
             <Title id={ titleId } title={ titles[titleId] } />
           </Col>
